412d8f932e2a7597a1171bd775528a7c567e6284
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2024-08-25 14:55:54 +0530

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,6 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0") // Gson converter
 
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
